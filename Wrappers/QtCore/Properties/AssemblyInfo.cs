3f8bb2add3e2c3ea8c68cd4805b49327ea34ac13
ric <garlicbready@googlemail.com>
2015-03-14 17:28:34 +0000

    added reference to CppSharp.Runtime, marked QtGUI and QtSerialPort as friend assemblies

--- Wrappers/QtCore/Properties/AssemblyInfo.cs
+++ Wrappers/QtCore/Properties/AssemblyInfo.cs
@@ -14,6 +14,11 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
+// Allow other Qt Assemblies freind level access to the class's within QtCore
+[assembly: InternalsVisibleTo("QtGUI")]
+[assembly: InternalsVisibleTo("QtSerialPort")]
+// TODO Add additional assemblies here
+
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
